bb0493a1a40471464d2fda1af11aea7a8f9cd35f
Merima Dedic <merimaxdedic@gmail.com>
2023-10-19 20:39:01 -0500

    Add index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+  
+  const messages = [];
+  for (const name of names) {
+    const message = `Thank you, ${name}, for the wonderful ${eventName} gift!`;
+    messages.push(message);
+    debugger;
+  }
+  
+
+  return messages;
+}
+let names = ['Sarah', 'Bob', 'Charlie'];
+let thankYouMessages = writeCards(names);
+console.log(thankYouMessages);
+
+function countDown(num) {
+    if (typeof num !== 'number' || num <= 0 || !Number.isInteger(num)) {
+      console.log('Please provide a positive integer.');
+      return;
+    }
+  
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+}
+
